71ba6ee227c0409cd6f1c6b54d8941044ad22099
Pomai Ahlo <85930736+PoAhlo@users.noreply.github.com>
2026-04-08 22:43:57 -0700

    Feature: Add Pentomino Data (#1)
    * Add main game scene
    
    * Add pentomino base script and resource files

---
+++ src/pentomino/PentominoData.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name PentominoData
+
+@export var name: String
+@export var texture: Texture2D
+@export var coordinates: Array[Vector2i] = [
+	Vector2i(0,0), Vector2i(0,0), Vector2i(0,0), Vector2i(0,0), Vector2i(0,0)
+]
