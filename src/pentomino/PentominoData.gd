c6f568e73303f68778806ac7ac835bd0308a471e
Pomai Ahlo <ahlo.pomai@gmail.com>
2026-04-09 22:50:29 -0700

    Fix piece mixup

--- src/pentomino/PentominoData.gd
+++ src/pentomino/PentominoData.gd
@@ -1,8 +1,8 @@
 extends Resource
 class_name PentominoData
 
-@export var name: String
-@export var texture: Texture2D
+@export var name: String = ""
+@export var color: Color = Color.WHITE
 @export var coordinates: Array[Vector2i] = [
 	Vector2i(0,0), Vector2i(0,0), Vector2i(0,0), Vector2i(0,0), Vector2i(0,0)
 ]
