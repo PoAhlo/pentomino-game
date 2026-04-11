ecdefae5ac38fc2e294d36f566e948efe31cf35a
Pomai Ahlo <ahlo.pomai@gmail.com>
2026-04-10 21:34:37 -0700

    Add new texture atlas for colored pentominos

--- src/pentomino/PentominoData.gd
+++ src/pentomino/PentominoData.gd
@@ -2,7 +2,8 @@ extends Resource
 class_name PentominoData
 
 @export var name: String = ""
-@export var color: Color = Color.WHITE
+# Coordinate for the texture in block_atlas.png
+@export var atlas_coords: Vector2i = Vector2i(0, 0)
 @export var coordinates: Array[Vector2i] = [
-	Vector2i(0,0), Vector2i(0,0), Vector2i(0,0), Vector2i(0,0), Vector2i(0,0)
+	Vector2i(0, 0), Vector2i(0, 0), Vector2i(0, 0), Vector2i(0, 0), Vector2i(0, 0)
 ]
