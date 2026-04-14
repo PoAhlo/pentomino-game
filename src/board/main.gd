c73be413d56b782aadb542b1411f8097211b3ed8
Pomai Ahlo <ahlo.pomai@gmail.com>
2026-04-13 21:00:13 -0700

    Add ability to cycle through pieces

--- src/board/main.gd
+++ src/board/main.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
-@export var active_data: PentominoData 
+@export var library: PentominoLibrary
+
 @onready var background_layer: TileMapLayer = $BackgroundLayer
 @onready var active_layer: TileMapLayer = $ActivePieceLayer
 
-var current_pos: Vector2i = Vector2i(5, 5)
+var active_piece_index: int = -1
+var active_piece_data: PentominoData
+var curr_pos: Vector2i = Vector2i(6, 3)
 var block_source_id: int = -1
 
 func _ready() -> void:
@@ -14,15 +17,23 @@ func _ready() -> void:
 	if ts.get_source_count() > 0:
 		block_source_id = ts.get_source_id(0)
 	
-	if active_data:
-		draw_piece(active_data, current_pos)
+	if library.pieces.size() > 0:
+		active_piece_index = 0
+		active_piece_data = library.pieces[active_piece_index]
+	
+	if active_piece_data:
+		draw_piece(active_piece_data, curr_pos)
 	else:
 		print("!!! WARNING: No PentominoData assigned to Main node in the Inspector.")
 
 func _input(event: InputEvent) -> void:
 	# A quick test: Press 'Space' to rotate the piece in place
-	if event.is_action_pressed("ui_accept"):
+	if event.is_action_pressed("rotate_clockwise"):
 		rotate_piece_clockwise()
+	elif event.is_action_pressed("ui_focus_next"):
+		change_piece(1)
+	elif event.is_action_pressed("ui_focus_prev"):
+		change_piece(-1)
 
 func draw_piece(data: PentominoData, pos: Vector2i) -> void:
 	# Clear the layer so we don't leave a trail of old blocks
@@ -37,11 +48,27 @@ func draw_piece(data: PentominoData, pos: Vector2i) -> void:
 		active_layer.set_cell(target_grid_pos, block_source_id, data.atlas_coords)
 
 func rotate_piece_clockwise() -> void:
-	# Traditional Tetris Matrix Rotation: (x, y) -> (-y, x)
+	# Coodinate Rotation: (x, y) -> (-y, x)
 	var rotated_coords: Array[Vector2i] = []
-	for c in active_data.coordinates:
+	for c in active_piece_data.coordinates:
 		rotated_coords.append(Vector2i(-c.y, c.x))
 	
 	# Update the coordinates in the resource and redraw
-	active_data.coordinates = rotated_coords
-	draw_piece(active_data, current_pos)
+	active_piece_data.coordinates = rotated_coords
+	draw_piece(active_piece_data, curr_pos)
+
+func change_piece(direction: int) -> void:
+	# update position
+	curr_pos = Vector2i(6, 3)
+	
+	# update index
+	active_piece_index = posmod(active_piece_index + direction, library.pieces.size())
+	active_piece_data = library.pieces[active_piece_index]
+	
+	draw_piece(active_piece_data, curr_pos)
+
+func is_pos_valid(test_coords: Array[Vector2i]) -> bool:
+	for mino in test_coords:
+		var mino_pos = test_pos + mino
+		
+		
